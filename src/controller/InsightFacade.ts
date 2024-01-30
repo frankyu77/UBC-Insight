26316595381e833df99d536089e8f40f7bc513b6
aqilf <afaizala@students.cs.ubc.ca>
2024-01-29 17:57:41 -0800

    Started implementing Query, Checking if input is JSON.

--- src/controller/InsightFacade.ts
+++ src/controller/InsightFacade.ts
@@ -1,4 +1,5 @@
 import {IInsightFacade, InsightDataset, InsightDatasetKind, InsightResult} from "./IInsightFacade";
+import * as string_decoder from "string_decoder";
 
 export default class InsightFacade implements IInsightFacade {
 	constructor() {
@@ -14,6 +15,19 @@ export default class InsightFacade implements IInsightFacade {
 	}
 
 	public async performQuery(query: unknown): Promise<InsightResult[]> {
+
+		// Check if it is a JSON file
+		if (typeof query !== "string") {
+			return Promise.reject("Not a string input."); // Not a string, can't be JSON
+		}
+
+		try {
+			// const jsonData = JSON.parse(input as string);
+			// return typeof jsonData === "object" && jsonData !== null;
+		} catch (error) {
+			return Promise.reject("Not av valid JSON."); // Not a string, can't be JSON
+		}
+
 		return Promise.reject("Not implemented.");
 	}
 
